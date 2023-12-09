27c94bbccbd611e76e142cdaede14b277c400648
sami5671 <samialam5671@gmail.com>
2023-12-10 01:11:03 +0600

    compare

---
+++ Basic/Compare1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+void positive_negative_zero(int number){
+    if (number > 0)
+    {
+       cout<< "Number is positive";
+    }else if (number < 0)
+    {
+        cout<< "Number is Negative";
+    }else{
+         cout<< "Number is Zero";
+    }
+    
+    
+}
+
+int main(){
+
+    int a;
+    cout<< "Enter a number: ";
+    cin>> a;
+    positive_negative_zero(a);
+
+    return 0;
+}
