3bebf6c198e4f4c595e26305543e9a2fe1b6c151
sami5671 <samialam5671@gmail.com>
2023-12-10 00:50:37 +0600

    simple interest

---
+++ Basic/Simple_interest.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    double principal, rateOfInterest, time, result;
+    cout<< "Enter the value of principal, rateOfInterest, time: ";
+    cin>> principal >> rateOfInterest >> time;
+
+    result = (principal*rateOfInterest*time)/100;
+    cout<<"The Simple interest is: " << result << "%"<< endl;
+
+    return 0; 
+}
