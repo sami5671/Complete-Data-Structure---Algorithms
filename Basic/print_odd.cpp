46d2c218e3c2db7fc7c223201043fdd181eaabdb
sami5671 <samialam5671@gmail.com>
2023-12-10 01:52:30 +0600

    print odd

--- Basic/print_odd.cpp
+++ Basic/print_odd.cpp
@@ -1,24 +1,24 @@
 // inclusive 
-
 #include<iostream> 
 using namespace std;
 
-
-void printNumber(int number){
+void printOdd(int number){
     int i = 1;
-     cout<<"The number from " << i << " to " << number << " is: ";
+     cout<<"The Odd number from " << i << " to " << number << " is: ";
     for ( i = 1; i <= number; i++)
     {
-        cout<< i << " ";
+        if (i % 2 == 1)
+        {
+            cout<< i << " ";
+        }
     }
-    
 }
 int main(){
 
     int n;
     cout<< "Enter the number: ";
     cin>>n;
-    printNumber(n);
+    printOdd(n);
 
     return 0;
 }
