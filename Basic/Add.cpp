490f906e18c32c8d9f67b0b232e7ea1c3c2b7621
sami5671 <samialam5671@gmail.com>
2023-12-10 00:40:36 +0600

    added the basic part

---
+++ Basic/Add.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a,b, sum;
+    cout<< "Printing Tow Given Number"<< endl;
+    cout<< "Enter number 1: ";
+    cin >> a;
+    cout<< "Enter number 2: ";
+    cin>> b;
+    sum = a + b;
+    cout<<"The of "<< a << " & " << b << " is: " << sum << endl;
+
+    return 0;
+}
