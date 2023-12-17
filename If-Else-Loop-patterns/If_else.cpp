1f444ff2c2f34ba47df14b67343336acce363f3e
sami5671 <samialam5671@gmail.com>
2023-12-17 18:25:44 +0600

    if else

---
+++ If-Else-Loop-patterns/If_else.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a = 9, b;
+    cout<< "Enter a number: "<< endl;
+    cin>> b;
+
+    if (b == a)
+    {
+        cout<< "Wow matched the number Win!!!" << endl;
+    }
+    else if (b < 0)
+    {
+        cout<< "Negative number"<< endl;
+    }else if (b== 0)
+    {
+    cout<< "Number is zero"<< endl;
+    }
+    else{
+        cout<< "Positive number"<< endl;
+    }
+    
+    
+    return 0;
+
+}
