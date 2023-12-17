5d515fe7a6b552adf38d1cd88c4f50ff95e4c872
sami5671 <samialam5671@gmail.com>
2023-12-17 18:52:44 +0600

    while loop

---
+++ If-Else-Loop-patterns/While_loop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a, b = 10;
+    cout<< "Enter a integer value: ";
+    cin >> a;
+
+    while (a <= b) //WHILE IT IS TRUE IT WILL PRINT THE MESSAGE
+    {
+        cout<< a << " ";
+        a++;
+    }
+
+    return 0;
+}
