2ae9494614daeee8b2fa7c93564b1f3cc08cf3f6
sami5671 <samialam5671@gmail.com>
2023-12-17 18:42:35 +0600

    Ascii value

---
+++ If-Else-Loop-patterns/Upper_Lower_number.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int ascii;
+    char ch;
+    cout<< "Enter a character or number or special character: ";
+    cin>> ch;
+
+    if (ch >= 65 && ch <= 90)
+    {
+        cout<< ch << " "  << "is Uppercase." << endl;
+    }else if (ch >= 97 && ch <= 122)
+    {
+       cout<< ch << " " << "is Lowercase." << endl;
+    }else if (ch >= 48 && ch <= 57)
+    {
+        cout<< ch << " " << "is a number"<< endl;
+    }else{
+        cout<< ch << " " << "is a special character." << endl;
+    }
+
+
+    return 0;
+}
