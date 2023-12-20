73b234a2f325337b4632e33e8ea8b49986df7b4f
sami5671 <samialam5671@gmail.com>
2023-12-20 16:48:35 +0600

    pattern

---
+++ If-Else-Loop-patterns/Pattern_17.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std; 
+int main(){
+    int n;
+    cout<< "Enter the number: ";
+    cin>>n;
+
+    int row =1;
+    while (row<=n)
+    {
+        int col = 1;
+        while (col<=row)
+        {
+            char ch = 'A' + row + col - 2;
+            cout<< ch << " ";
+            col++;
+        }
+        cout<< endl;
+        row++;
+    }
+    
+
+
+    return 0;
+}
