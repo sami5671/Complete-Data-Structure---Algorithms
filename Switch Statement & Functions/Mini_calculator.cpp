cf0838aa82e736809d46349a8125a909ea14cb91
sami5671 <samialam5671@gmail.com>
2023-12-27 11:26:28 +0600

    mini calculator

---
+++ Switch Statement & Functions/Mini_calculator.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a, b, option;
+    cout<< "Enter the value of a & b: ";
+    cin>>a >> b;
+
+    cout<< "Which case would you like to do:\n\n1.Addition\n2.Subtract\n3.Multiply\n4.Divide: ";
+    cin>>option;
+
+    cout<< endl;
+
+    switch (option)
+    {
+    case 1:
+       cout<< "Addition of "<< a << " And " << b << " is: "<< a + b << endl;
+        break;
+    case 2:
+        cout<< "Subtraction of "<< a << " And " << b << " is: "<< a - b << endl;
+        break;
+    case 3:
+        cout<< "Multiplication of "<< a << " And " << b << " is: "<< a * b << endl;
+        break;
+    case 4:
+        cout<< "Division of "<< a << " And " << b << " is: "<< a / b << endl;
+        break;
+    
+    default:
+        break;
+    }
+
+    return 0;
+}
