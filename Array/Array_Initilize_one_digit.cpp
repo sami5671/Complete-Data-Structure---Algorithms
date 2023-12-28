a5b638636d460662b8cac2f941f76e3570a460c5
sami5671 <samialam5671@gmail.com>
2023-12-28 11:40:36 +0600

    array

---
+++ Array/Array_Initilize_one_digit.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+// =================================================================
+void initialize_Array(){
+    int arr[10];
+    for (int i = 0; i < 10; i++)
+    {
+        arr[i] = 5;
+    }
+    for (int i = 0; i < 10; i++)
+    {
+       cout<< arr[i] << " ";
+    }
+    
+}
+//================================================================
+
+
+int main(){
+    
+
+    initialize_Array();
+
+    return 0;
+}
