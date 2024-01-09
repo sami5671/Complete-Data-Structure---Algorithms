3ff547c47e03b3252012344c9ba3f177a2c06def
sami5671 <samialam5671@gmail.com>
2024-01-09 21:31:40 +0600

    codingninja

---
+++ Coding Ninja/Learing Basic/SwitchCase.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+double areaSwitchCase(int ch, vector<double> a) {
+	// Write your code here
+
+	switch(ch){
+		case 1: 
+		return M_PI*a[0]*a[0];
+		break;
+
+		case 2: 
+		return  a[0]* a[1];
+		break;
+	}
+
+}
+
+
+
+int main(){
+    
+ 
+    return 0;
+}
