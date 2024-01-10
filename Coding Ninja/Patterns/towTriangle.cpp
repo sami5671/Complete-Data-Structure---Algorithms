403296d9ca80006b83ef901b1e46f92e9bc8ce3a
sami5671 <samialam5671@gmail.com>
2024-01-10 21:15:00 +0600

    coding ninja

---
+++ Coding Ninja/Patterns/towTriangle.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  for (int i = 0; i < n; i++)
+  {
+        for (int j = 0; j <= i ; j++)
+        {
+        cout<< "* ";
+        }
+        for (int  k = 0; k < n-i+1 ; k++)
+        {
+        cout<< " ";
+        }
+    cout<< endl;
+  }
+
+//   2nd triangle 
+  for (int i = 1; i < n; i++)
+  {
+        for (int j = 0; j < n-i ; j++)
+        {
+        cout<< "* ";
+        }
+        for (int  k = 0; k < i; k++)
+        {
+        cout<< " ";
+        }
+    cout<< endl;
+  }
+  
+
+  return 0;
+}
