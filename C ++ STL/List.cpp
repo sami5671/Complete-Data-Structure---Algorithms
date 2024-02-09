74da536821af3675a37644fa365f208df9da754e
sami5671 <samialam5671@gmail.com>
2024-02-09 21:32:22 +0600

    list STL

---
+++ C ++ STL/List.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<list>
+using namespace std;
+
+int main(){
+
+    list<int> l;
+
+    list<int> n(5,100);
+      for (int i: n)
+    {
+        cout<< i << " ";
+    }
+
+cout<< endl;
+
+    l.push_back(10);
+    l.push_front(20);
+
+   l.erase(l.begin());
+    for (int i: l)
+    {
+        cout<< i << " ";
+    }
+    
+
+    return 0; 
+}
