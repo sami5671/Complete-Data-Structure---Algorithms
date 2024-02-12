fdb8769dcb39b1b5315620462da9094c4ac5f225
sami5671 <samialam5671@gmail.com>
2024-02-12 17:51:42 +0600

    reverse array using vectoer

--- Problem_Solving/Reverse_array.cpp
+++ Problem_Solving/Reverse_array.cpp
@@ -1,42 +1,45 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-int reverse_array(int arr[], int size){
+vector<int> reverse_array(vector<int> v){
 
     int start = 0;
-    int end = size -1;
+    int end = v.size() - 1;
 
    for (int start = 0; start < end; start++, end--)
    {
-     swap(arr[start], arr[end]);
+     swap(v[start], v[end]);
    }
-    // while (start < end)
-    // {
-    //       swap(arr[start], arr[end]);
-    //       start++;
-    //       end--;
-    // }
+    return v;
 }
 
-void print_array(int arr[], int size){
+void print_array(vector<int> v){
 
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < v.size(); i++)
     {
-        cout<< arr[i] << " ";
+        cout<< v[i] << " ";
     }
 }
 
 int main (){
 
-    int arr[5] = {1,2,3,4,5};
+    vector<int> v;
+
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(40);
+    v.push_back(30);
+    v.push_back(50);
+
 
     cout<< "Original Array: ";
-    print_array(arr, 5);
+    print_array(v);
+
     cout<< endl;
     cout<< "Reversed Array: ";
-    reverse_array(arr, 5);
-    print_array(arr, 5);
-
+    vector<int> reverse = reverse_array(v);
+    print_array(reverse);
 
     return 0;
 }
