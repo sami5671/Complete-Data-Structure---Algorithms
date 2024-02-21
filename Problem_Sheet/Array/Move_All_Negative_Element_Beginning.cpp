b092b2aa14bf730fe7681ea756d0779c4f564e2e
sami5671 <samialam5671@gmail.com>
2024-02-21 22:31:22 +0600

    Dutch national flag algorithm

--- Problem_Sheet/Array/Move_All_Negative_Element_Beginning.cpp
+++ Problem_Sheet/Array/Move_All_Negative_Element_Beginning.cpp
@@ -1,9 +1,7 @@
 #include<iostream>
 using namespace std;
 
-
-
-// if we sort the array to move the element in the beginning then its a naive approach
+// if we sort the array to move the negative element in the beginning then its a naive approach
 void Move_Element_Beginning(int arr[], int size){
 
         for (int i = 0; i < size; i++)
@@ -16,7 +14,25 @@ void Move_Element_Beginning(int arr[], int size){
                 }
             }
         }
-        
+}
+
+// we can use Dutch national flag Algorithm for rearrangement the negative elements in the beginning
+
+void Arrange_Element(int arr[], int size){
+    int start = 0;
+    int end = size -1;
+    while (start < end)
+    {
+       if (arr[start] < 0)
+       {
+         start++;
+       }else if (arr[end] > 0)
+       {
+         end--;
+       }else{
+        swap(arr[start], arr[end]);
+       }
+    }
 }
 
 void Print_array(int arr[], int size){
@@ -28,11 +44,12 @@ void Print_array(int arr[], int size){
 
 int main(){
    
-   int arr[100] = {-12, 11, -13, -5, 6, -7, 5, -3, -6};
+   int arr[100] = {1, 2,  -4, -5, 2, -7, 3, 2, -6, -8, -9, 3, 2,  1};
    int size;
    cin >> size;
 
-   Move_Element_Beginning(arr, size);
+//    Move_Element_Beginning(arr, size);
+   Arrange_Element(arr, size);
    Print_array(arr, size);
 
    return 0;
