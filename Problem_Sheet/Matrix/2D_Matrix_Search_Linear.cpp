d5e4753445babd972d14b0463567fc34d3eaff70
sami5671 <samialam5671@gmail.com>
2024-02-25 19:20:57 +0600

    Linear search in 2D array

--- Problem_Sheet/Matrix/2D_Matrix_Search_Linear.cpp
+++ Problem_Sheet/Matrix/2D_Matrix_Search_Linear.cpp
@@ -1,6 +1,22 @@
 #include<iostream>
 using namespace std;
 
+bool Linear_Search(int arr[][100], int row_size, int col_size, int search_Value){
+
+        for (int i = 0; i < row_size; i++)
+        {
+            for (int j = 0; j < col_size; j++)
+            {
+                 if (arr[i][j] == search_Value)
+                 {
+                    return true;
+                 }
+                 
+            }
+            
+        }
+        return false;
+}
 
 void print_array(int arr[][100], int row_size, int column_size){
 
@@ -25,6 +41,18 @@ int main(){
        
     };
     print_array(arr, 4, 3);
+
+
+    // finding the search value
+
+    if ( Linear_Search(arr, 4, 3, 100))
+    {
+       cout << "Search value Found" << endl;
+    }
+    else{
+        cout << "Not Found" << endl;
+    }
+    
 
 
     return 0;
