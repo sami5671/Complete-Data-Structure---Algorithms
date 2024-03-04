f7bc074e34f5e386367f62543ef0715557b7e153
sami5671 <samialam5671@gmail.com>
2024-03-04 10:17:08 +0600

    Check the number is Armstrong or not

---
+++ Basic/Armstrong_Numers.cpp
@@ -0,0 +1,60 @@
+// digit ar sum is equal to the original number
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+// ---------------------Functions-------------------------------------------
+
+int count_digit(int n){
+
+    int count = 0;
+
+    while (n > 0)
+    {
+        n = n / 10;
+        count++;
+    }
+    return count;
+}
+
+bool check_Armstrong(int n){
+
+    int sumOfPower = 0;
+    int count = count_digit(n);
+
+        while (n > 0)
+        {
+            int digit = n % 10;
+            n = n / 10;
+            sumOfPower = sumOfPower + pow(digit, count);
+        }
+
+
+    if (sumOfPower == n)
+    {
+        return true;
+    }else{
+        return false;
+    }
+    
+}
+// ----------------------------------------------------------------
+
+int main(){
+
+    int n;
+    cout<< "Enter the Number: ";
+    cin >> n;
+
+    if (check_Armstrong(n))
+    {
+       cout<< "Armstrong Number" << endl;
+    }else{
+         cout<< "Not Armstrong Number" << endl;
+    }
+    
+
+
+    return 0;
+}
