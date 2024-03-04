39234ae90368516c2c606a25693692865d47d85f
sami5671 <samialam5671@gmail.com>
2024-03-04 11:39:25 +0600

    sumOfDivisors

---
+++ Problem_Solving/Sum_Of_All_divisor.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+
+
+int Print_divisor_sum(int number){
+
+int sum = 0;
+
+    for (int i = 1; i <= number; i++)
+    {
+        if (number % i == 0)
+        {
+            sum = sum + i;
+        }
+    }
+    return sum;
+}
+
+
+int main(){
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+
+    int result  =  0;
+
+     for (int i = 1; i <= n; i++)
+     {
+        result = result + Print_divisor_sum(i);
+     }
+     
+    cout << result << endl;
+   
+    return 0;
+}
