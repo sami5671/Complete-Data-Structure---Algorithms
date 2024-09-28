10cb8a22825084563798b3b87e831fe6c7a0c0e6
sami5671 <samialam5671@gmail.com>
2024-09-28 11:46:06 +0600

    6 types of pattern

---
+++ 4_New_Sheet/Patterns/P5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    for (int row = 1; row <= n; row++)
+    {
+        for (int col = n; col >= row; col--)
+        {
+           cout << " *";
+        }
+        cout << endl;
+    }
+}
